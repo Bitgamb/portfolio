241b2974a6a9d2642f673879f35e392134b5ddf7
Soham <sohammokal420@gmail.com>
2025-03-15 21:45:58 +0530

    new figures

--- components/canvas/models/PlayerModel.jsx
+++ components/canvas/models/PlayerModel.jsx
@@ -1,33 +1,48 @@
-function PlayerModel({ nodes, materials, scale, position, rotation, group }) {
+import { useGLTF } from '@react-three/drei';
+import { useEffect, useState } from 'react';
+import { useFrame } from '@react-three/fiber';
+
+function PlayerModel({ scale, position, rotation, group }) {
+    const modelPaths = [
+        "/models/player/noddy.gltf",
+        "/models/player/gojo.gltf",
+        "/models/player/nezuko.gltf",
+         "/models/player/thomas.gltf"
+    ];
+
+    const [currentModelIndex, setCurrentModelIndex] = useState(0);
+    const { scene } = useGLTF(modelPaths[currentModelIndex]);
+
+    // Change model every 5 seconds
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentModelIndex((prevIndex) => (prevIndex + 1) % modelPaths.length);
+        }, 10000); // Change model every 5 seconds
+
+        return () => clearInterval(interval);
+    }, []);
+
+    // Auto-rotate the model
+    useFrame(({ clock }) => {
+        if (scene) {
+            scene.rotation.y = clock.getElapsedTime() * 0.8; // Adjust speed if needed
+        }
+    });
+
+    if (!scene) {
+        console.error("GLTF not loaded correctly");
+        return null;
+    }
+
     return (
-        <group dispose={null} scale={scale} position={position} ref={group} rotation={rotation}>
-            <group>
-            <primitive object={nodes.Hips} />
-                <skinnedMesh
-                    frustumCulled={false}
-                    geometry={nodes.avaturn_body.geometry}
-                    material={materials.avaturn_body_material}
-                    skeleton={nodes.avaturn_body.skeleton}
-                />
-                <skinnedMesh
-                    frustumCulled={false}
-                    geometry={nodes.avaturn_hair_0.geometry}
-                    material={materials.avaturn_hair_0_material}
-                    skeleton={nodes.avaturn_hair_0.skeleton}
-                />
-                <skinnedMesh
-                    frustumCulled={false}
-                    geometry={nodes.avaturn_look_0.geometry}
-                    material={materials.avaturn_look_0_material}
-                    skeleton={nodes.avaturn_look_0.skeleton}
-                />
-                <skinnedMesh
-                    frustumCulled={false}
-                    geometry={nodes.avaturn_shoes_0.geometry}
-                    material={materials.avaturn_shoes_0_material}
-                    skeleton={nodes.avaturn_shoes_0.skeleton}
-                />
-            </group>
+        <group
+            dispose={null}
+            scale={scale || [1, 1, 1]}
+            position={position || [0, -1, -1]}
+            rotation={[0, 0, 0]}
+            ref={group}
+        >
+            <primitive object={scene} />
         </group>
     );
 }
